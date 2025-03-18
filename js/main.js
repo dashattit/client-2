5ec937a555e53c9fa00220dc36156b7e3e86fec1
dashattit <dashyla.2021@mail.ru>
2025-03-18 15:49:48 +0700

    template for node-card and column

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
+// компонент column
+Vue.component('column', {
+    // данные
+    props: ['title', 'notes', 'maxNotes', 'isLocked', 'allowAdd'],
+    // шаблон
+    template: `
+    <div class="column" :class="{ 'locked': isLocked }">
+        <h2>{{ title }}</h2>
+        <note-card v-for="(note, index) in notes"
+                :note="note"
+                :key="note.id"
+                @update-note="$emit('update-note', index)"
+                @move-note="$emit('move-note', index)"
+        </note-card>
+        <button @click="$emit('add-note')" v-if="allowAdd" :disabled="notes.length >= maxNotes || isLocked">Добавить карточку</button>
+    </div>
+    `,
+});
+
 // компонент node-card
 Vue.component('node-card', {
     props: ['note'],
+    // шаблон
     template: `
     <div class="node-card">
         <h3>{{ note.title }}</h3>
@@ -12,6 +32,7 @@ Vue.component('node-card', {
                 </label>
             </li>
         </ul>
+        <p v-if="note.completedDate">Завершено: </p>
     </div>  
     `,
     //метод для обработки изменения пункта
@@ -23,23 +44,57 @@ Vue.component('node-card', {
     }
 });
 
-// компонент column
-Vue.component('column', {
-    // данные
-    props: ['title', 'notes', 'maxNotes', 'isLocked', 'allowAdd'],
-    // шаблон
+let App = ({
     template: `
-    <div class="column" :class="{ 'locked': isLocked }">
-        <h2>{{ title }}</h2>
-        <note-card v-for="note in notes"
-                :note="note"
-                :key="note.id"
-                @update-note="$emit('update-note', index)"
-                @move-note="$emit('move-note', index)"
-        </note-card>
+    <div class="columns">
+        <column
+            title="Столбец 1"
+            :notes="firstColumnNotes"
+            :maxNotes="maxNotes"
+            :isLocked="isFirstColumnLocked"
+            :allowAdd="true"
+            @update-note="updateNote"
+            @move-note="moveNote"
+            @add-note="addNote"
+        ></column>
+        <column
+            title="Столбец 2"
+            :notes="secondColumnNotes"
+            :maxNotes="5"
+            :isLocked="false"
+            :allowAdd="false"
+            @update-note="updateNote"
+            @move-note="moveNote"
+        ></column>
+        <column
+            title="Столбец 3"
+            :notes="thirdColumnNotes"
+            :maxNotes="Infinity"
+            :isLocked="false"
+            :allowAdd="false"
+            @update-note="updateNote"
+            @move-note="moveNote"
+        ></column>
     </div>
     `,
-});
+    data() {
+        return {
+            notes: null,
+            isFirstColumnLocked: false,
+        }
+    },
+    computed: {
+        firstColumnNotes() {
+            return this.notes.filter(note => note.column === 1);
+        },
+        secondColumnNotes() {
+            return this.notes.filter(note => note.column === 2);
+        },
+        thirdColumnNotes() {
+            return this.notes.filter(note => note.column === 3);
+        }
+    }
+})
 
 let vue = new Vue({
     el: '#app',
